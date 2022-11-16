a6c5965bee0d545293c86a96f4f03156b9f66f51
JeNeSuisPasAndrew <permandrey2000@mail.ru>
2022-11-16 21:10:07 +0500

    Число сочетаний через одномерную матрицу + тесты

--- double_recursion_next.py
+++ double_recursion_next.py
@@ -2,30 +2,28 @@ from statistics import mean
 import time
 import numpy as np
 import DoubleRecursion as dr
-import math
 
 
-def find_via_td_matrix(n, k):
+def num_of_combs_via_td_matrix(n, k):
 	if n < k:
 		raise Exception('n < k')
-	
-	if k == 0:
+	if n < 0 or k < 0:
+		raise Exception('n or k < 0')
+
+	if k == 0 or k == n:
 		return 1
 	elif k == 1:
 		return n
-	elif k == n:
-		return 1
-	
+
 	if n == 0 and k != 0:
 		return 0
-	
 
 	matrix = np.zeros((k+1, n+1))
 	np.fill_diagonal(matrix, 1)
 	matrix[0] = 1
 	for col in range(n+1):
 		matrix[1][col] = col
-	
+
 	for row in range(k+1):
 		try:
 			matrix[row][row+1] = row + 1
@@ -39,7 +37,7 @@ def find_via_td_matrix(n, k):
 	n_cur = 4
 	for row in range(k-1):
 		n_cur_local = n_cur
-		
+
 		c_left_up = matrix[k_cur-1][n_cur_local-1]
 		c_left = matrix[k_cur][n_cur_local-1]
 
@@ -48,40 +46,72 @@ def find_via_td_matrix(n, k):
 			c_left = matrix[k_cur][n_cur_local]
 			n_cur_local += 1
 			c_left_up = matrix[k_cur-1][n_cur_local-1]
-			
-		
+
 		k_cur += 1
 		n_cur += 1
-	
+
 	return matrix[k][n]
+
+
+def num_of_combs_via_od_matrix(n, k):
+	if n < k:
+		raise Exception('n < k')
+	if n < 0 or k < 0:
+		raise Exception('n or k < 0')
+
+	if k == 0 or k == n:
+		return 1
+	elif k == 1:
+		return n
+
+	if n == 0 and k != 0:
+		return 0
+
+	num_of_combs = 1
+	while k > 0:
+		num_of_combs *= n / k
+		k -= 1
+		n -= 1
+
+	return num_of_combs
+
 
 if __name__ == '__main__':
 	task = int(input('1.Тесты\n2.Проверка скорости итерации и рекурсии\n'))
 	test_num = int(input('Кол-во тестов = '))
-	
+
 	success_num = 0
-	iter_times = []
+	iter_td_times = []
+	iter_od_times = []
 	rec_times = []
 
 	for _ in range(test_num):
-		k = np.random.randint(0, 10)
-		n = k + np.random.randint(0, 5)
-		
+		k = np.random.randint(0, 50)
+		n = k + np.random.randint(0, 20)
+
 		start = time.perf_counter()
-		a = find_via_td_matrix(n, k)
+		a = num_of_combs_via_td_matrix(n, k)
 		end = time.perf_counter()
-		iter_times.append(end-start)
-		
+		iter_td_times.append(end-start)
+
+		start = time.perf_counter()
+		c = num_of_combs_via_od_matrix(n, k)
+		end = time.perf_counter()
+		iter_od_times.append(end-start)
+
 		start = time.perf_counter()
 		b = dr.recur_relation(n, k)
 		end = time.perf_counter()
 		rec_times.append(end-start)
-		
-		if abs(dr.recur_relation(n, k) - find_via_td_matrix(n, k)) <= 0.1:
+
+		# Рекурсивный алгоритм сбивает точность, поэтому создано данное условие
+		if abs(dr.recur_relation(n, k) - num_of_combs_via_od_matrix(n, k)) <= 0.1 \
+			and abs(dr.recur_relation(n, k) - num_of_combs_via_td_matrix(n, k)) <= 0.1:
 			success_num += 1
 		else:
 			print(f'{n=}, {k=}')
-			print(f'iter={a}')
+			print(f'iter_od={c}')
+			print(f'iter_td={a}')
 			print(f'rec={b}')
 			print()
 
@@ -89,11 +119,12 @@ if __name__ == '__main__':
 		case 1:
 			print(f'Success tests:{success_num}/{test_num}')
 			print(f'Accuracy:{success_num/test_num*100}%')
-		
+
 		case 2:
-			print(f'Iter mean time = {mean(iter_times)}')
+			print(f'Iter od mean time = {mean(iter_od_times)}')
+			print(f'Iter td mean time = {mean(iter_td_times)}')
 			print(f'Rec mean time = {mean(rec_times)}')
-			
+			print(f'Fastest: {min(mean(iter_od_times), mean(iter_td_times), mean(rec_times))}')
+
 		case _:
 			print('Не то вы нажали .__.')
-
---
+++ test.py
@@ -0,0 +1,4 @@
+from double_recursion_next import num_of_combs_via_od_matrix
+from timeit import Timer
+
+Timer('combinations(4, 2)').timeit()
